76d7b9f2ff0a13521b30fe9e12b3a2ba8c2d93af
CP <chandrapratap321@gmail.com>
2021-04-15 23:52:47 +0530

    corrects service.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/e-restaurant"));
+const appPath = path.join(__dirname, "dist", "e-restaurant");
+app.use(express.static(appPath));
 
-app.get("/*", (req, res) =>
-  res.sendFile("index.html", { root: "dist/e-restaurant/" })
-);
+// app.get("/*", (req, res) => res.sendFile("index.html", { root: appPath }));
+app.get("/*", (req, res) => res.sendFile(path.join(appPath, index.html)));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
