5e7646229704b7c58418f209a246e126d9f88ffa
CP <chandrapratap321@gmail.com>
2021-04-18 00:01:39 +0530

    corrects routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { Iuser } from './Modules/user/modal/interfaces/Iuser.interface';
-import { fetchUser } from './Modules/user/store/actions/user.action';
-import { userSelector } from './Modules/user/store/selectors/user.selector';
 import { AuthService } from './services/auth.service';
 
 @Component({
@@ -13,21 +9,6 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'e-restaurant';
-  constructor(
-    private authSerive: AuthService,
-    private router: Router,
-    private store: Store
-  ) {}
-  ngOnInit() {
-    if (
-      this.authSerive.isUserLoggedIn() &&
-      this.authSerive.getUserRole() === 'user'
-    )
-      this.router.navigate(['/shop']);
-    if (
-      this.authSerive.isUserLoggedIn() &&
-      this.authSerive.getUserRole() === 'admin'
-    )
-      this.router.navigate(['/admin']);
-  }
+  constructor(private authSerive: AuthService, private router: Router) {}
+  ngOnInit() {}
 }
--- src/app/components/entry/entry.component.ts
+++ src/app/components/entry/entry.component.ts
@@ -1,11 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css'],
 })
 export class EntryComponent implements OnInit {
-  constructor() {}
+  constructor(private router: Router, private authSerive: AuthService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    console.log(this.router.url);
+    if (
+      this.authSerive.isUserLoggedIn() &&
+      this.authSerive.getUserRole() === 'user'
+    )
+      this.router.navigate(['/shop']);
+    if (
+      this.authSerive.isUserLoggedIn() &&
+      this.authSerive.getUserRole() === 'admin'
+    )
+      this.router.navigate(['/admin']);
+  }
 }
